62ecdb641f6c3f520163199ef28362b52e82d844
Ike Odira <odiraike8@gmail.com>
2024-07-12 04:59:36 +0100

    display hello world extension

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+// import './App.css'
+
+function App() {
+
+  const handleClick = async () => {
+    let [tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id! },
+      func: () => {
+        alert("Hello from my extension!");
+      }
+
+    })
+  }
+
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button onClick={handleClick}>Say Hello</button>
+    </div>
+
+  )
+}
+
+export default App
